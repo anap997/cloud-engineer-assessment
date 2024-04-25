74baf048076c72e74e7583e12eec333722529513
anap997 <167522327+anap997@users.noreply.github.com>
2024-04-25 09:43:26 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,87 +1,32 @@
 provider "aws" {
-  region = "us-east-1" # Change to your desired region
+  region = "eu-central-1"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "todo_vpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
   enable_dns_hostnames = true
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "todo_igw" {
+  vpc_id = aws_vpc.todo_vpc.id
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.main.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.todo_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # Change to match desired availability zone
+  availability_zone = "eu-central-1a"
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b" # Change to match desired availability zone
-}
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.todo_vpc.id
 
-resource "aws_security_group" "lambda_sg" {
-  name        = "lambda_sg"
-  description = "Allow HTTP access to Lambda function"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.todo_igw.id
   }
 }
 
-resource "aws_lambda_function" "my_lambda_function" {
-  function_name = "my_lambda_function"
-  description   = "My .NET Lambda Function"
-  runtime       = "dotnetcore3.1"
-  handler       = "MyNamespace::MyClass::MyMethod" # Change to your handler
-  filename      = "path/to/your/lambda_function.zip" # Path to your Lambda function code
-  source_code_hash = filebase64sha256("path/to/your/lambda_function.zip") # Calculate hash of your Lambda function code
-
-  role          = aws_iam_role.lambda_role.arn # Add this line
-
-  vpc_config {
-    subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-    security_group_ids = [aws_security_group.lambda_sg.id]
-  }
-}
-
-
-resource "aws_api_gateway_rest_api" "my_api" {
-  name = "My API"
-}
-
-resource "aws_api_gateway_resource" "my_api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part   = "myapp"
-}
-
-resource "aws_api_gateway_method" "my_api_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.my_api_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "my_api_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.id
-  resource_id             = aws_api_gateway_resource.my_api_resource.id
-  http_method             = aws_api_gateway_method.my_api_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_lambda_function.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "my_api_deployment" {
-  depends_on = [aws_api_gateway_integration.my_api_integration]
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "prod"
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
